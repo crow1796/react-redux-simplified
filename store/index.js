4766c1ef609365150ed88b5738d98aea7fed1af5
Joshua <joshuatundag@gmail.com>
2019-09-12 20:52:29 +0800

    update store

--- store/index.js
+++ store/index.js
@@ -1,4 +1,10 @@
+import axios from 'axios';
 import reducers from './reducers';
-import { createStore } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+axios.defaults.baseURL = '<base_api_url>';
 
-export default createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(ReduxThunk.withExtraArgument(axios))));
+export default store;
